583ba011bf08d1584a37694ee5a27906e65b2bc0
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2025-01-15 14:37:48 +0100

    updated docs and exercise related R-scripts to work on slurm

--- docs/cluster_howto/exercise_R/magic_cal_3.R
+++ docs/cluster_howto/exercise_R/magic_cal_3.R
@@ -1,3 +1,12 @@
+tdir <- tempdir()
+.libPaths(tdir)
+
+# check if the magic library is among the installed packages
+hasmagic    <- c("magic") %in% rownames(installed.packages())
+
+# if not then install it in a temporary directory (which has been added to the library path)
+if (hasmagic==FALSE)    {install.packages("magic",   lib=tdir, dependencies=TRUE, repos="https://cran.r-project.org")}
+
 # load libraries 
 library("magic")
 
@@ -17,5 +26,3 @@ save_objects(c("magic_matrix"), path=NA, job_bookkeep_info=jinfo, append=TRUE)
 sum_diagonal <- sum(diag(magic_matrix))
 save_objects(c("sum_diagonal"), path=NA, job_bookkeep_info=jinfo, append=TRUE)
 
-# save data objects to output file
-#save_objects(c("magic_matrix","sum_diagonal"), path=NA, job_bookkeep_info=jinfo)
